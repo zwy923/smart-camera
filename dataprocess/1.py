fb627c8feba5a4804645dde13152feaea5292075
Wenyue Zhang <811739729@qq.com>
2023-12-12 18:34:28 +0200

    1.1

--- dataprocess/1.py
+++ dataprocess/1.py
@@ -1,42 +1,62 @@
 import cv2
-import yolov8
+from ultralytics import YOLO
+import numpy as np
 
-class VideoCamera(object):
-    def __init__(self, camera_source=0):
-        # 使用笔记本摄像头作为视频源 (0 通常是默认摄像头)
-        # 后续可以更换为外部摄像头
-        self.video = cv2.VideoCapture(camera_source)
-        self.yolo_model = yolov8.load_model()  # 加载 YOLOv8 模型
+def process_frame(frame):
 
-    def __del__(self):
-        self.video.release()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    return gray_frame
 
-    def get_frame(self):
-        success, image = self.video.read()
-        if success:
-            # 进行对象检测
-            results = self.yolo_model.detect(image)
-            # 处理结果
-            image = self.process_results(image, results)
-            # 返回处理后的图像
-            return image
-        else:
-            return None
+def is_significant_diff(diff, threshold=60):
 
-    def process_results(self, image, results):
-        # 这里可以添加处理检测结果的代码，例如绘制边界框、标记等
-        # ...
-        return image  # 返回处理后的图像
+    return np.mean(diff) > threshold
 
-def main():
-    camera = VideoCamera()
+def save_and_recognize(frame, model):
+
+    cv2.imwrite("diff_frame.jpg", frame)
+    results = model(frame)
+    for r in results:
+        print(r.boxes)
+
+
+def capture_video(source, model):
+    cap = cv2.VideoCapture(source)
+    prev_frame = None
+
     while True:
-        frame = camera.get_frame()
-        if frame is not None:
-            # 显示帧
-            cv2.imshow('Video', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-if __name__ == '__main__':
+        current_frame = process_frame(frame)
+
+        if prev_frame is not None:
+            diff = cv2.absdiff(current_frame, prev_frame)
+            if is_significant_diff(diff):
+                save_and_recognize(frame, model)  # 使用原始帧以保留更多细节
+
+        prev_frame = current_frame
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def load_model(model_path):
+
+    try:
+        model = YOLO(model_path)
+        return model
+    except Exception as e:
+        print(f"Model loading failed: {e}")
+        return None
+    
+def main():
+    model_path = './yolov8n.pt'
+    model = load_model(model_path)
+    if model is None:
+        return
+
+    source = 0  # 默认使用电脑内置摄像头
+    capture_video(source, model)
+
+if __name__ == "__main__":
     main()
